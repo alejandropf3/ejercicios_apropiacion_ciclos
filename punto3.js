1d567dabbcb6dd56c004c20693a7af21649b9c9d
alejandropf3 <alejandropefa31@gmail.com>
2025-11-06 17:40:30 -0500

    Creamos el archivo punto3 y desarrollamos el ejercicio 3 del punto B

---
+++ punto3.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 10; i++) //iniciamos un bucle que contara hasta 10
+{
+    let numero = prompt("Ingresa un numero") //Solicitamos el ingreso del nuemero
+    if (numero > 50) //Condicional que determina si el numero es mayor a 50
+    {
+        alert("El numero: "+numero+" es mayor que 50") // Imprimir el numero mas mensaje
+    }
+
+}
