1e4ba84cd9b0e6dff8ab6dbadf5e5d3776c6f740
Jaider Esparza <xenno2003@gmail.com>
2025-11-06 15:26:35 -0500

    Desarrollamos el ejercicio 1 del punto C

---
+++ punto1.js
@@ -0,0 +1,8 @@
+// Inicia un bucle que contará desde el 0 hasta el 20.
+for (let i = 0; i < 20;i++) 
+{
+    if (i % 3 == 0) // Verifica si el número actual 'i' es divisible exactamente por 3.
+    {
+        alert(i+" Es multiplo de 3") //Imprimir mensaje 
+    }
+}
